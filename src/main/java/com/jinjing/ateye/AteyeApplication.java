2ca06967f5a92f816d4e67b46d6bda8b4376c00a
金靖 <jinjing@jinjingdeMacBook-Pro.local>
2018-07-26 17:21:44 +0800

    init springboot env

---
+++ src/main/java/com/jinjing/ateye/AteyeApplication.java
@@ -0,0 +1,12 @@
+package com.jinjing.ateye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AteyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AteyeApplication.class, args);
+	}
+}
